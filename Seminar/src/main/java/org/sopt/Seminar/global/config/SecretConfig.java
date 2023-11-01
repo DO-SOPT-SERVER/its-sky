f60a0815935b5284cc9013eba09bd967fdb5e552
its-sky <smc9919@naver.com>
2023-11-01 22:56:58 +0900

    remove: 불필요한 파일 삭제

--- Seminar/src/main/java/org/sopt/Seminar/global/config/SecretConfig.java
+++
@@ -1,8 +0,0 @@
-package org.sopt.Seminar.global.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-//@PropertySource(value = {"$classpath:/information.properties"})
-public class SecretConfig {
-}
