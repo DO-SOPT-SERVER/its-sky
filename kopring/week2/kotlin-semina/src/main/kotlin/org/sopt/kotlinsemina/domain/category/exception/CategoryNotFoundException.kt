cd2d09b1e204be01066d069494929f58c1dc4523
its-sky <smc9919@naver.com>
2023-11-06 23:30:34 +0900

    refactor: custom exception static import 적용

--- kopring/week2/kotlin-semina/src/main/kotlin/org/sopt/kotlinsemina/domain/category/exception/CategoryNotFoundException.kt
+++ kopring/week2/kotlin-semina/src/main/kotlin/org/sopt/kotlinsemina/domain/category/exception/CategoryNotFoundException.kt
@@ -1,6 +1,6 @@
 package org.sopt.kotlinsemina.domain.category.exception
 
-import org.sopt.kotlinsemina.global.common.exception.ErrorType.*
+import org.sopt.kotlinsemina.global.common.exception.ErrorType.NotFound.*
 import org.sopt.kotlinsemina.global.common.exception.SoptException
 
-class CategoryNotFoundException: SoptException.NotFoundException(NotFound.CATEGORY_NOT_FOUND, "해당하는 카테고리가 없습니다.")
+class CategoryNotFoundException: SoptException.NotFoundException(CATEGORY_NOT_FOUND, "해당하는 카테고리가 없습니다.")
--- kopring/week2/kotlin-semina/src/main/kotlin/org/sopt/kotlinsemina/domain/post/exception/PostNotFoundException.kt
+++ kopring/week2/kotlin-semina/src/main/kotlin/org/sopt/kotlinsemina/domain/post/exception/PostNotFoundException.kt
@@ -1,6 +1,6 @@
 package org.sopt.kotlinsemina.domain.post.exception
 
-import org.sopt.kotlinsemina.global.common.exception.ErrorType
+import org.sopt.kotlinsemina.global.common.exception.ErrorType.NotFound.*
 import org.sopt.kotlinsemina.global.common.exception.SoptException
 
-class PostNotFoundException: SoptException.NotFoundException(ErrorType.NotFound.POST_NOT_FOUND, "해당 게시물이 존재하지 않습니다.")
+class PostNotFoundException: SoptException.NotFoundException(POST_NOT_FOUND, "해당 게시물이 존재하지 않습니다.")
