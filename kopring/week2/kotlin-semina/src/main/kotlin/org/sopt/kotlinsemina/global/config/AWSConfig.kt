a3bc19bad914f29604d7ce97216823dff143901b
its-sky <smc9919@naver.com>
2023-12-01 02:21:48 +0900

    feat: AWS Config 구현

---
+++ kopring/week2/kotlin-semina/src/main/kotlin/org/sopt/kotlinsemina/global/config/AWSConfig.kt
@@ -0,0 +1,40 @@
+package org.sopt.kotlinsemina.global.config
+
+import org.sopt.kotlinsemina.global.common.dto.AwsProperty
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.auth.credentials.SystemPropertyCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3Client
+
+@Configuration
+class AWSConfig(
+    val awsProperty: AwsProperty
+) {
+
+    @Bean
+    fun systemPropertyCredentialsProvider(): SystemPropertyCredentialsProvider {
+        System.setProperty(AWS_ACCESS_KEY_ID, awsProperty.accessKey)
+        System.setProperty(AWS_SECRET_ACCESS_KEY, awsProperty.secretKey)
+        return SystemPropertyCredentialsProvider.create()
+    }
+
+    @Bean
+    fun getRegion(): Region {
+        return Region.of(awsProperty.region)
+    }
+
+    @Bean
+    fun getS3Client(): S3Client {
+        return S3Client.builder()
+            .region(getRegion())
+            .credentialsProvider(systemPropertyCredentialsProvider())
+            .build()
+    }
+
+    companion object {
+        private const val AWS_ACCESS_KEY_ID = "aws.accessKeyId"
+        private const val AWS_SECRET_ACCESS_KEY = "aws.secretAccessKey"
+    }
+}
